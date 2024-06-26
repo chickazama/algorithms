c636910ee9e9cecba413d0e2c8db9ce331b09abf
Matthew Hope <chickazama@gmail.com>
2024-06-26 12:48:19 +0100

    Create recursive merge sort implementation

--- sort/merge/merge.go
+++ sort/merge/merge.go
@@ -20,6 +20,54 @@ func main() {
 	data := core.Convert(argv)
 	fmt.Println("Input:")
 	fmt.Println(data)
+	data = mergeSort(data)
 	fmt.Println("Output:")
 	fmt.Println(data)
+}
+
+func mergeSort(data []int) []int {
+	n := len(data)
+	if n < 2 {
+		return data
+	}
+	var left []int
+	var right []int
+	for i, v := range data {
+		if i < n/2 {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
+		}
+	}
+	left = mergeSort(left)
+	right = mergeSort(right)
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	var ret []int
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] <= right[0] {
+			ret = append(ret, left[0])
+			if len(left) > 1 {
+				left = left[1:]
+			} else {
+				left = []int{}
+			}
+		} else {
+			ret = append(ret, right[0])
+			if len(right) > 1 {
+				right = right[1:]
+			} else {
+				right = []int{}
+			}
+		}
+	}
+	if len(left) > 0 {
+		ret = append(ret, left...)
+	}
+	if len(right) > 0 {
+		ret = append(ret, right...)
+	}
+	return ret
 }
